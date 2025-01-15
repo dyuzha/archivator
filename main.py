bca0ed927d68322984034085f62bb3681172a4d7
dyuzha <matvey10d@gmail.com>
2025-01-15 07:56:52 +0300

    Разделил конфиг на 2 конфига
    congig.yml --> rc.yml
               |
               --> options.yml

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 from models import ResultsFolder, TemplateProcessor, YamlHandler
 from pathlib import Path
 
-yml_handler = YamlHandler("config.yml")
-yml_handler.load_data()
 
-paths = yml_handler.data["paths"]
-folder_name = yml_handler.data["folder name"]
-templates = yml_handler.data["templates list"]
+rc_handler = YamlHandler("rc.yml")
+rc_handler.load_data()
+rc_data = rc_handler.data
 
-TEMPLATES = Path(paths["templates"])
-DATA = Path(paths["data"])
-ARCHIVES_DATA = Path(paths["target"])
+TEMPLATES = Path(rc_data["templates"])
+DATA = Path(rc_data["data"])
+ARCHIVES_DATA = Path(rc_data["target"])
 
+options_handler = YamlHandler("options")
+options_handler.load_data()
+options_data = options_handler.data
+
+folder_name = options_data["folder name"]
+templates = options_data["templates list"]
+compression = options_data["compression"]
+allow_zip64 = options_data["allow_zip64"]
 
 def main():
     results_folder = ResultsFolder(ARCHIVES_DATA)
     results_folder.create_folder(folder_name)
 
-    processor = TemplateProcessor ( 
+    processor = TemplateProcessor (
         templates_folder=TEMPLATES,
         data_folder=DATA,
         target_folder=results_folder.path_folder
