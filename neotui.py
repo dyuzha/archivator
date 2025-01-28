e9648ceb5fbf3149a72ac0c166984c96597b27e9
dyuzha <matvey10d@gmail.com>
2025-01-28 08:04:37 +0300

    ..

--- neotui.py
+++ neotui.py
@@ -1,69 +1,75 @@
 from models import Processor
 import npyscreen
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
 
 @dataclass
 class Options:
     folder_name: str = "Bases"
-    selected_templates: list = []
+    selected_templates: list = field(default_factory=list)
 
 
 class AllowForm(npyscreen.ActionForm):
     def create(self):
-        folder_name = self.add(npyscreen.TitleText,
-                                    name = "Folder name:",
-                                    value = "")
+        self.show_atx = 20
+        self.show_aty = 10
 
-        self.options = Options(folder_name=folder_name.value)
+        folder_name = self.add(
+                npyscreen.TitleText,
+                name = "Folder name:",
+                value =self.parentApp.options.folder_name
+                )
+        # selected_templates = self.add(npyscreen.)
+
+        # self.options = Options(folder_name=folder_name.value)
+        # self.options = Options(selected_templates=selected_templates.value)
 
     def on_ok(self):
-        self.parentApp.options = self.options
-        self.parentApp.switchForm('TEMPLATES')
+        # self.parentApp.options = self.options
+        pass
+
+    def afterEditing(self):
+        self.parentApp.setNextForm(None)
 
 
 class OptionsForm(npyscreen.ActionForm):
     def create(self):
-        folder_name = self.add(npyscreen.TitleText,
+        self.folder_name = self.add(npyscreen.TitleText,
                                     name = "Folder name:",
                                     value = "")
 
-        self.options = Options(folder_name=folder_name.value)
-
     def on_ok(self):
-        self.parentApp.options = self.options
-        self.parentApp.switchForm('TEMPLATES')
+        self.parentApp.options.folder_name = self.folder_name.value
+        self.parentApp.setNextForm('TEMPLATES')
 
 
 class TemplatesForm(npyscreen.ActionForm):
     def create(self):
         templates = list(self.parentApp.processor.get_templates())
         self.selected_templates = self.add(npyscreen.TitleMultiSelect,
-                                   name = "Templates:",
                                    values=templates)
     def on_ok(self):
-        self.parentApp.options.selected_templates = self.selected_templates
-        self.parentApp.switchForm('ALLOW')
+        # self.parentApp.options.selected_templates = self.selected_templates.values
+        pass
+
+    def afterEditing(self):
+        self.parentApp.setNextForm("ALLOW")
 
 
 class App(npyscreen.NPSAppManaged):
-    @property
-    def options(self):
-        return self._options
-
-    @options.setter
-    def options(self, value):
-        self._options = value
-
     def build_dir(self):
-        dir_name = self.options.dir_name
+        dir_name = self.options.folder_name
         templates = self.options.selected_templates
         self.processor.build_target_dir(dir_name, *templates)
 
     def onStart(self):
         self.processor = Processor("rc.yml")
-        self.addForm("MAIN", OptionsForm, name="Options")
+        self.options = Options()
+
+        self.addForm("MAIN", OptionsForm, name="Options",
+                     lines = 10, column = 10)
         self.addForm("TEMPLATES", TemplatesForm, name="Templates")
-        self.addForm("ALLOW", AllowForm, name="Allow")
+        self.addForm("ALLOW", AllowForm, name="Allow", lines = 10)
 
 
 if __name__ == '__main__':
--- old_tui.py
+++ old_tui.py
@@ -4,7 +4,7 @@ import npyscreen as np
 COMPRESSIONS = [
     "ZIP_DEFLATED",
     "ZIP_BZIP2",
-    "ZIP_LZMA", 
+    "ZIP_LZMA",
     "ZIP_STORED"
 ]
 
@@ -18,17 +18,17 @@ class MyApp(np.NPSAppManaged):
         self.interface.options.folder_name = opts["folder_name"]
         self.interface.options.compression = opts["compression"]
         self.interface.create_archives(*templates)
-        
+
 # This form class defines the display that will be presented to the user.
 
 class MainForm(np.ActionForm):
     def create(self):
         templates = self.parentApp.interface.get_templates()
-        self.folder_name = self.add(np.TitleText, 
-                                     name = "Folder name:", 
+        self.folder_name = self.add(np.TitleText,
+                                     name = "Folder name:",
                                      value= "")
 
-        self.compression = self.add(np.TitleSelectOne, 
+        self.compression = self.add(np.TitleSelectOne,
                                     max_height=4,
                                     name = "Compression",
                                     values=COMPRESSIONS)
@@ -40,17 +40,16 @@ class MainForm(np.ActionForm):
     def on_ok(self):
         # Создать архив
         self.parentApp.build_archive(
-            folder_name = self.folder_name.value, 
-            compression = self.compression.get_selected_objects()[0], 
+            folder_name = self.folder_name.value,
+            compression = self.compression.get_selected_objects()[0],
             *self.templates.get_selected_objects())
 
     def on_cancel(self):
         pass
- 
+
     def afterEditing(self):
         self.parentApp.setNextForm(None)
 
 if __name__ == '__main__':
     TA = MyApp()
     TA.run()
-
